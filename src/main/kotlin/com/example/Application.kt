0ea3e47ce42bd9c04cc30f96fff643344a811004
Lubomir1998 <lubcho_sk8@abv.bg>
2022-04-26 18:18:16 +0300

    Update stuff

--- src/main/kotlin/com/example/Application.kt
+++ src/main/kotlin/com/example/Application.kt
@@ -10,13 +10,17 @@ import kotlinx.serialization.json.Json
 
 fun main() {
     embeddedServer(Netty, port = 1926, host = "0.0.0.0") {
-        configureRouting()
-        install(ContentNegotiation) {
-            json(Json {
-                prettyPrint = true
-                isLenient = true
-                encodeDefaults = true
-            })
-        }
+        module()
     }.start(wait = true)
+}
+
+fun Application.module() {
+    configureRouting()
+    install(ContentNegotiation) {
+        json(Json {
+            prettyPrint = true
+            isLenient = true
+            encodeDefaults = true
+        })
+    }
 }
